1aede9cbf1adfe934c12916d5ff91711b5866fdc
faustojc <faustojcboko@gmail.com>
2022-10-26 11:38:24 +0800

    Changed the button text

--- js/frameVisible.js
+++ js/frameVisible.js
@@ -1,4 +1,11 @@
-function visible() {
-    let iframeVisible = document.getElementById("iframe_visible");
-    iframeVisible.style.visibility = visible;
+function iframeVisible() {
+    let x = document.getElementById('iframe_visible').style.getPropertyValue('visiblity');
+    
+    if (x == 'visible') {
+        document.getElementById('iframe_visible').css('visiblity', 'hidden');
+    }
+    else {
+        document.getElementById('iframe_visible').css('visiblity', 'visible');
+    }
 }
+
